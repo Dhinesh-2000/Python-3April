d2bcb6bba3641e18161a59d097a14647f75737e7
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-04-29 17:20:56 +0530

    test

---
+++ python/functions/Decorators/two.py
@@ -0,0 +1,24 @@
+def home_page(name,is_login):
+    print("Home Page")
+
+def order_page(name,is_login):
+    print("Order page")
+
+def product_page(name,is_login):
+    print("Product page")
+
+def profile_page(name,is_login):
+    print("Profile page")
+
+#Rahul
+home_page("Rahul",False)
+order_page("Rahul",False)
+product_page("Rahul",False)
+profile_page("Rahul",False)
+
+#sonia
+
+home_page("Sonia",True)
+order_page("Sonia",True)
+product_page("Sonia",True)
+profile_page("Sonia",True)
