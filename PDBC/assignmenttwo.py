ca92bb00cb95b9c0534012c18f099cebc8b523db
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-05-22 21:08:41 +0530

    test

--- PDBC/assignmenttwo.py
+++ PDBC/assignmenttwo.py
@@ -1,1 +1,20 @@
 import requests
+from pymongo import MongoClient
+
+resp=requests.get('https://jsonplaceholder.typicode.com/users')
+user_list=resp.json()
+
+try:
+    client=MongoClient('mongodb://localhost:27017/')
+    print("MongoDB connection successfully")
+    db=client['10am']
+    col=db['users']
+    col.insert_many(user_list)
+    print("Data Inserted Successfully")
+except:
+    Print("Read the err and rectify with patience")
+finally:
+    col=None
+    db=None
+    client=None
+
